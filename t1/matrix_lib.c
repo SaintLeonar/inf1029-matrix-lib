c8929691f2c6d3630775189eab9d9a4a040d02a1
LuizSeibel <82062376+LuizSeibel@users.noreply.github.com>
2022-08-27 18:35:26 -0300

    Add files via upload

--- t1/matrix_lib.c
+++ t1/matrix_lib.c
@@ -10,7 +10,7 @@ float *rows;
 // Cabeca das Funcoes 
 int scalar_matrix_mult(float scalar_value, struct matrix *matrix);
 int matrix_matrix_mult(struct matrix *matrixA, struct matrix * matrixB, struct matrix * matrixC);
-
+void test_matrix(float result_value, struct matrix *matrix);
 
 // Codigo 
 int main(){
@@ -26,12 +26,14 @@ int scalar_matrix_mult(float scalar_value, struct matrix *matrix){
     int tam;
     tam = matrix->height * matrix->width;
 
+    // aloca dinamicamente
     matrix->rows = (float*)malloc(tam*sizeof(float));
     if(matrix ->rows == NULL){
         printf("Erro de memoria insuficiente\n");
         return 0;
     }
 
+    // multiplica por escalar
     for(int i = 0; i < tam; i++){
         matrix->rows[i] *= scalar_value;
     }
@@ -42,3 +44,20 @@ int scalar_matrix_mult(float scalar_value, struct matrix *matrix){
 int matrix_matrix_mult(struct matrix *matrixA, struct matrix * matrixB, struct matrix * matrixC){
 
  }
+
+// Funcao para testar matriz
+void test_matrix(float result_value, struct matrix *matrix){
+
+    int tam;
+    tam = matrix->height * matrix->width;
+
+    printf("Testanto a matriz...\n");
+    // Percorre a matriz e testa se o valor esta igual ao resultado esperado
+    for(int i = 0; i < tam; i++){
+        if(matrix->rows[i] != result_value){
+            printf("Erro no calculo da matriz\n");
+            break;
+        }
+    }
+    printf("Matrix funcionando como o esperado\n");
+}
