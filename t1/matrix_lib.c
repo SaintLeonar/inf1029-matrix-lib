d3ac47f3210a0a18094667ae356dc91cc068438a
LuizSeibel <82062376+LuizSeibel@users.noreply.github.com>
2022-08-28 22:27:09 -0300

    Add files via upload

--- t1/matrix_lib.c
+++ t1/matrix_lib.c
@@ -23,7 +23,7 @@ int main(){
 // Funcao de multiplicação por escalar
 int scalar_matrix_mult(float scalar_value, struct matrix *matrix){
 
-    int tam;
+    unsigned long int tam;
     tam = matrix->height * matrix->width;
 
     // aloca dinamicamente
@@ -34,7 +34,7 @@ int scalar_matrix_mult(float scalar_value, struct matrix *matrix){
     }
 
     // multiplica por escalar
-    for(int i = 0; i < tam; i++){
+    for(unsigned long int i = 0; i < tam; i++){
         matrix->rows[i] *= scalar_value;
     }
     return 1;
@@ -43,17 +43,50 @@ int scalar_matrix_mult(float scalar_value, struct matrix *matrix){
 // Funcao de multiplicacao por matriz
 int matrix_matrix_mult(struct matrix *matrixA, struct matrix * matrixB, struct matrix * matrixC){
 
+    unsigned long int tamA, tamB, tamC;
+    tamA = matrixA->height * matrixA->width;
+    tamC = matrixA->height * matrixB->width;
+
+
+    matrixC->rows = (float*)malloc(tamC*sizeof(float));
+    if(matrixC->rows == NULL){
+        printf("Erro de memoria insuficiente\n");
+        return 0;
+    }
+
+    // Percorre a matriz C
+    for(unsigned long i = 0; i < tamC; i++){
+
+        //Percorre a matriz A
+        for(unsigned long int j = 0; j < tamA; j++){
+
+            // passa o valor da matrizA para valA (multiplicar a matriz B)
+            float valA = matrixA->rows[j];
+
+            // Percorre as colunas da matriz B para multiplicação
+            for(unsigned long int k = 0; k < matrixB->height; k++){
+                
+                // Percorre a matrizB
+                for(unsigned long int l = 0; l < matrixB->width; l++){
+                    float valB = matrixB->rows[(k * l) -1];
+                    matrixC->rows[i + l] += valA * valB;
+                }
+                
+            }
+        }
+    }
+    return 1;
  }
 
 // Funcao para testar matriz
 void test_matrix(float result_value, struct matrix *matrix){
 
-    int tam;
+    unsigned long int tam;
     tam = matrix->height * matrix->width;
 
     printf("Testanto a matriz...\n");
     // Percorre a matriz e testa se o valor esta igual ao resultado esperado
-    for(int i = 0; i < tam; i++){
+    for(unsigned long int i = 0; i < tam; i++){
         if(matrix->rows[i] != result_value){
             printf("Erro no calculo da matriz\n");
             break;
