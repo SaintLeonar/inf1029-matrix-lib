d7654fbd878e84ecbdf086f0d62b97eada956058
LuizSeibel <82062376+LuizSeibel@users.noreply.github.com>
2022-08-30 20:16:11 -0300

    Add files via upload

--- t1/matrix_generator.c
+++ t1/matrix_generator.c
@@ -1,13 +1,28 @@
 # include <stdio.h>
 #include <stdlib.h>
+#define TAM 1048576
+#define VAL 2.0
+// 1048576 - 1024 x 1024
+// 4194304 - 2048 x 2048
+
 
 int main(void){
-float vet[9] = {2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0};
+//float vet[9] = {2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0};
+
+
+float *vet;
+
+vet = (float*)malloc(TAM*sizeof(float));
+
+for(int i=0; i<TAM; i++){
+    vet[i] = VAL;
+}
+
 FILE * fp;
 
 fp = fopen ("teste.dat", "wb");
    
-fwrite(vet, sizeof(vet), 9, fp);
+fwrite(vet, sizeof(vet), TAM, fp);
 fclose(fp);
 printf("fim\n");
 
@@ -15,12 +30,17 @@ printf("fim\n");
 
 fp = fopen ("teste.dat", "rb");
 
-float array[9];
+float array[TAM];
 
 fread(array, sizeof(array), 1, fp);
 
-for(int i = 0; i < 9; i++)
+for(int i = 0; i < TAM; i++){
+    if(i > 256){
+        printf("- Passou de 256 -");
+        break;
+    }
     printf("%f ", array[i]);
+}
 printf("\n");
 
 fclose(fp);
