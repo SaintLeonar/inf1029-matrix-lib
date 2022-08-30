38b015c21009d17551bf79d9b1ba002f24e789dd
LuizSeibel <82062376+LuizSeibel@users.noreply.github.com>
2022-08-30 20:02:08 -0300

    Add files via upload

--- t1/matrix_generator.c
+++ t1/matrix_generator.c
@@ -2,14 +2,31 @@
 #include <stdlib.h>
 
 int main(void){
-float vet[9] = {1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0};
+float vet[9] = {2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0};
 FILE * fp;
 
 fp = fopen ("teste.dat", "wb");
    
-fwrite(vet, sizeof(float), 9, fp);
+fwrite(vet, sizeof(vet), 9, fp);
 fclose(fp);
 printf("fim\n");
+
+
+
+fp = fopen ("teste.dat", "rb");
+
+float array[9];
+
+fread(array, sizeof(array), 1, fp);
+
+for(int i = 0; i < 9; i++)
+    printf("%f ", array[i]);
+printf("\n");
+
+fclose(fp);
 return 0;
+
+
+
 }
 
