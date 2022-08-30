2db2449250e779a1bc0778325c0ddb00b98b0ce7
LuizSeibel <82062376+LuizSeibel@users.noreply.github.com>
2022-08-30 17:34:29 -0300

    Add files via upload

--- t1/matrix_lib.c
+++ t1/matrix_lib.c
@@ -11,23 +11,23 @@ float *rows;
 int scalar_matrix_mult(float scalar_value, struct matrix *matrix);
 int matrix_matrix_mult(struct matrix *matrixA, struct matrix * matrixB, struct matrix * matrixC);
 void test_matrix(float result_value, struct matrix *matrix);
+void show_matrix(struct matrix *matrix, char title);
 
 // Codigo 
 int main(){
 
     // teste matriz 3x3
+    /*
     struct matrix matrixA, matrixB;
 
     printf("%p - A\n", matrixA.rows);
     printf("%p - B\n", matrixB.rows);
-
 
     matrixA.rows = (float*)malloc(9*sizeof(float));
     if(matrixA.rows == NULL){
         printf("Erro de memoria insuficiente\n");
         return 0;
     }
-
 
     matrixA.height = 3;
     matrixA.width = 3;
@@ -41,7 +41,6 @@ int main(){
         return 0;
     }
 
-
     printf("Matriz\n");
     for(int i = 0; i < 9; i++){
         printf("%f ", matrixA.rows[i]);
@@ -51,19 +50,11 @@ int main(){
     matrixB.height = 3;
     matrixB.width = 3;
 
-
-    
-
-
     for(int i = 0; i < 9; i++){
         matrixB.rows[i] = 2;
         printf("%f ", matrixB.rows[i]);
     }
     printf("\n");
-    
-    
-
-
 
     //scalar_matrix_mult(3.0, &matrixA);
 
@@ -75,7 +66,8 @@ int main(){
 
     struct matrix matrixC;
 
-    matrix_matrix_mult(&matrixA, &matrixB, &matrixC);
+    matrix_matrix_mult(&matrixA, &matrixB, &matrixC);*/
+
     return 0;
 }
 
@@ -84,18 +76,11 @@ int main(){
 // Funcao de multiplicação por escalar (Falta o retorno 0)
 int scalar_matrix_mult(float scalar_value, struct matrix *matrix){
 
+    // declaração de variaveis
     unsigned long int tam;
     tam = matrix->height * matrix->width;
     
-
-    /*float* temp_array;
-    temp_array = (float*)malloc(tam*sizeof(float));
-    if(temp_array == NULL){
-        printf("Erro de memoria insuficiente\n");
-        return 0;
-    }*/
-
-    // multiplica por escalar
+    // Percorre o vetor e multiplica pelo escalar
     for(unsigned long int i = 0; i < tam; i++){
         matrix->rows[i] *= scalar_value;
     }
@@ -106,21 +91,37 @@ int scalar_matrix_mult(float scalar_value, struct matrix *matrix){
 // Funcao de multiplicacao por matriz
 int matrix_matrix_mult(struct matrix *matrixA, struct matrix * matrixB, struct matrix * matrixC){
 
-    unsigned long int tamA, tamB, tamC;
+    // declaração das variaveis das matrizes A e B
+    unsigned long int tamA, tamB;
     tamA = matrixA->height * matrixA->width;
-    tamC = matrixA->height * matrixB->width;
+    tamB = matrixB->height * matrixB->width;
+    
+    // testa se as structs estão preenchidas
+    if(tamA == 0 || matrixA->rows == 'NULL'){
+        printf("Erro na struct da matriz A");
+        return 0;
+    }
+    if(tamB == 0 || matrixB->rows == 'NULL'){
+        printf("Erro na struct da matriz B");
+        return 0;
+    }
 
     // monta a matriz C
-    matrixC->height = matrixA->height;
+    unsigned long int tamC;
+    tamC = matrixA->height * matrixB->width;
+    
+    /*matrixC->height = matrixA->height;
     matrixC->width = matrixB->width;
+
+
 
     matrixC->rows = (float*)malloc(tamC*sizeof(float));
     if(matrixC->rows == NULL){
         printf("Erro de memoria insuficiente\n");
         return 0;
     }
-    // inicializa a matriz com zeros
-    printf("Matriz C\n");
+
+    printf("Matriz C\n");*/
 
     // Percorre a matriz C
     for(unsigned long i = 0; i < tamC; i++){
@@ -130,10 +131,8 @@ int matrix_matrix_mult(struct matrix *matrixA, struct matrix * matrixB, struct m
             printf("%f ", matrixC->rows[i]);
         }
     }
-    printf("aaa\n");
-    for(int i = 0; i < tamC; i++)
-        printf("%f ",matrixC->rows[i]);
-    printf("\n");
+    show_matrix(&matrixC, 'C');
+
     return 1;
  }
 
@@ -148,8 +147,25 @@ void test_matrix(float result_value, struct matrix *matrix){
     for(unsigned long int i = 0; i < tam; i++){
         if(matrix->rows[i] != result_value){
             printf("Erro no calculo da matriz\n");
-            break;
+            return;
         }
     }
     printf("Matrix funcionando como o esperado\n");
+}
+
+void show_matrix(struct matrix *matrix, char title){
+
+    unsigned long int tam;
+    tam = matrix->height * matrix->width;
+
+    printf("---------- matrix %c ----------\n");
+
+    for(int i = 0; i < tam; i++){
+        if(i > 256){
+            printf("\n -- A matriz passou do limite de 256 --");
+            break;
+        }
+        printf("%f ",matrix->rows[i]);
+    }
+    printf("\n");
 }
