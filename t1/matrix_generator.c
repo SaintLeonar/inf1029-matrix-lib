c8a4219e661899f7ca5054628bdae2604af6f94b
LuizSeibel <82062376+LuizSeibel@users.noreply.github.com>
2022-08-30 21:21:15 -0300

    Add files via upload

--- t1/matrix_generator.c
+++ t1/matrix_generator.c
@@ -1,7 +1,7 @@
 # include <stdio.h>
 #include <stdlib.h>
 #define TAM 1048576
-#define VAL 2.0
+#define VAL 1.0
 // 1048576 - 1024 x 1024
 // 4194304 - 2048 x 2048
 
@@ -23,12 +23,13 @@ FILE * fp;
 fp = fopen ("matrix.dat", "wb");
    
 fwrite(vet, sizeof(vet), TAM, fp);
+free(vet);
 fclose(fp);
 printf("-- Matriz Gerada --\n");
 
 
 
-fp = fopen ("teste.dat", "rb");
+fp = fopen ("matrix.dat", "rb");
 
 float array[TAM];
 
