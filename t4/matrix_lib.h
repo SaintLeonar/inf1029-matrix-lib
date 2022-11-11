cd72db3f9c5ffb884502b2ac2c9cdb6fc9bf955a
LuizSeibel <82062376+LuizSeibel@users.noreply.github.com>
2022-11-11 17:42:46 -0300

    Add files via upload

--- t4/matrix_lib.h
+++ t4/matrix_lib.h
@@ -1,11 +1,11 @@
-//extern int qtd_threads;
-
 typedef struct {
     unsigned long int height;
     unsigned long int width;
-    float *rows;
+    float *h_rows;
+    float *d_rows;
+    int alloc_mode;
 } Matrix;
 
 int scalar_matrix_mult(float scalar_value, Matrix *matrix);
 int matrix_matrix_mult(Matrix *matrixA, Matrix * matrixB, Matrix * matrixC);
-void set_number_threads(int num_threads);
+int set_grid_size(int threads_per_block, int max_blocks_per_grid);
