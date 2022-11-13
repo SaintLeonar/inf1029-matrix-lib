f2f86d7dd7eb69853e813d65ce3c0d2307ad2535
LuizSeibel <82062376+LuizSeibel@users.noreply.github.com>
2022-11-13 18:01:09 -0300

    Add files via upload

--- t4/matrix_lib.cu
+++ t4/matrix_lib.cu
@@ -6,6 +6,7 @@
 
 int set_grid_size(int threads_per_block, int max_blocks_per_grid){
 
+    return 1;
 }
 
 __global__
@@ -52,6 +53,63 @@ int scalar_matrix_mult(float scalar_value, Matrix *matrix){
         printf("cudaMemcpy (device to host): returned error %s (code %d)\n", cudaGetErrorString(cudaError), cudaError);
         return 0;
         }
+
+  return 1;
+}
+
+__global__
+void kernel_matrix_matrix_mult(Matrix matrixA, Matrix matrixB, Matrix matrixC){
+    int largura = blockIdx.x * blockDim.x + threadIdx.x;
+    int altura = blockIdx.y * blockDim.y + threadIdx.y;
+
+    float soma = 0;
+
+    if(largura < THREADS_PER_BLOCK && altura < THREADS_PER_BLOCK)
+        for(int i = 0; i < THREADS_PER_BLOCK; i++)
+            matrixC.d_rows[largura * THREADS_PER_BLOCK + altura] += matrixA.d_rows[largura * THREADS_PER_BLOCK + i] * matrixB.d_rows[i * THREADS_PER_BLOCK + altura];
+}
+
+
+int matrix_matrix_mult(Matrix *matrixA, Matrix * matrixB, Matrix * matrixC){
+
+    int blockSize, numBlocks;
+    cudaError_t cudaError;
+
+    unsigned long int tamA, tamB, tamC;
+
+    tamA = matrixA->width * matrixA->height;
+    if(tamA == 0 || matrixA->d_rows == NULL || matrixA->h_rows == NULL) return 0;
+    tamB = matrixB->width * matrixB->height;
+    if(tamB == 0 || matrixB->d_rows == NULL || matrixB->h_rows == NULL) return 0;
+    tamC = matrixC->width * matrixC->height;
+    if(tamC == 0 || matrixC->d_rows == NULL || matrixC->h_rows == NULL) return 0;
+
+    // Copia a matrix de host para device
+    cudaError = cudaMemcpy(matrixA->d_rows, matrixA->h_rows, tamA*sizeof(float), cudaMemcpyHostToDevice);
+    if (cudaError != cudaSuccess) {
+	printf("cudaMemcpy - A (host to device): returned error %s (code %d)\n", cudaGetErrorString(cudaError), cudaError);
+        return 0;
+    }
+
+    cudaError = cudaMemcpy(matrixB->d_rows, matrixB->h_rows, tamB*sizeof(float), cudaMemcpyHostToDevice);
+    if (cudaError != cudaSuccess) {
+	printf("cudaMemcpy - B (host to device): returned error %s (code %d)\n", cudaGetErrorString(cudaError), cudaError);
+        return 0;
+    }
+
+   blockSize = THREADS_PER_BLOCK;
+   numBlocks = (tamC + blockSize - 1) / blockSize;
+   kernel_matrix_matrix_mult<<<numBlocks, blockSize>>>(*matrixA, *matrixB, *matrixC);
+
+    cudaDeviceSynchronize();
+
+    cudaError = cudaMemcpy(matrixC->h_rows, matrixC->d_rows, tamC*sizeof(float), cudaMemcpyDeviceToHost);
+
+    if (cudaError != cudaSuccess)
+    {
+        printf("cudaMemcpy - C (device to host): returned error %s (code %d), line(%d)\n", cudaGetErrorString(cudaError), cudaError, __LINE__);
+        return 1;
+    }
 
   return 1;
 }
